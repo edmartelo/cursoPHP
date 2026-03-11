e390ea75bf2e3f46a3e5f99238817a4c80b378b3
edmartelo <smarteloedu@gmail.com>
2026-03-11 11:29:33 -0300

    update

---
+++ 7_estrutura_de_repeticao/1_while/index.php
@@ -0,0 +1,36 @@
+<?php
+
+  // $x = 0;
+  // while($x < 10) {
+  //   echo "$x <br>";
+  //   $x++;
+  // }
+
+  // echo "<br>";
+
+  // $y = 0;
+  // while($y < 10) {
+  //   echo "$y <br>";
+  //   $y += 2;
+  // }
+  
+  // echo "<br>";
+
+  // $z = 10;
+  // while($z > 0) {
+  //   echo "$z <br>";
+  //   $z--;
+  // }
+
+  // echo "<br>";
+
+  $a = 0;
+  while($a < 10) {
+    // corpo do looping
+    if($a % 2 != 0) {
+      echo "$a <br>";
+    }
+
+    // incremento do contador
+    $a++;
+  }
