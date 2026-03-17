8e7d7a5eb53fbbcba0db67342157b0a01c2173ba
edmartelo <smarteloedu@gmail.com>
2026-03-17 15:54:22 -0300

    update

---
+++ 7_estrutura_de_repeticao/10_exercicio/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+$valor = $arr[$i];
+
+for($i = 0; $i < 10; $i++) {
+  echo "teste de valor $valor <br>";
+}
---
+++ 7_estrutura_de_repeticao/9_for/index.php
@@ -0,0 +1,13 @@
+<?php
+
+// contador; condição; incremento/decremento
+
+$nome = "Eduardo";
+
+for($i = 0; $i < 10; $i++) {
+  if($i == 5) {
+    echo "$nome <br>";
+  }
+
+  echo "contador $i <br>";
+}
