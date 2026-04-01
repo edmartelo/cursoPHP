dc47aec7884b403b49c4f67fea3cfdf116f17a2f
edmartelo <smarteloedu@gmail.com>
2026-04-01 15:37:54 -0300

    update

---
+++ 11_arrays/1_adicionando_a_arrays/index.php
@@ -0,0 +1,43 @@
+<?php
+
+
+// adicionando valores
+$arr = [];
+
+print_r($arr);
+echo "<br>";
+
+
+$arr[0] = 10;
+print_r($arr);
+echo "<br>";
+
+
+$arr[1] = 15;
+print_r($arr);
+echo "<br>";
+
+
+
+
+// modificando valores
+
+$arr[1] += 15;
+print_r($arr);
+echo "<br>";
+
+
+$arrAssoc = [];
+print_r($arrAssoc);
+echo "<br>";
+
+$arrAssoc["Modelo"] = "Palio";
+print_r($arrAssoc);
+echo "<br>";
+
+$arrAssoc["Marca"] = "Fiat";
+print_r($arrAssoc);
+echo "<br>";
+
+
+
